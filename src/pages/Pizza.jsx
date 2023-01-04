32cac2e417c75deba4dcb9ef34ebc4fd55068031
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-01-04 15:54:26 -0300

    Alteração em pizza.jsx

--- src/pages/Pizza.jsx
+++ src/pages/Pizza.jsx
@@ -5,21 +5,33 @@ import './pizza.css';
 import '../App.css';
 
 class Pizza extends React.Component {
+  state = {
+    product: ''
+  }
+
+  handleclick= ({target}) =>{
+   this.setState({
+    product: target.value
+   })
+  }
   render () {
+    const { product } = this.state
     return (
     <div className='conteiner-full'>
       <Header />
       <div>
+        <input type="text" value={product} id='product' name='product' onChange={this.handleclick}/>
+        <button onAuxClick={this.handleclick}>Pesquisar</button>
       <p>Pizzas</p>
       <section className='conteiner-products'>
         {
-          Pizzas.map((pizza) => (
+          Pizzas.filter((pedido)=> pedido.name.toLowerCase().includes(product.toLowerCase())).map((pizza) => (
             <div className='item'>
               <p>{pizza.name}</p>
               <img src={pizza.image} alt="imagemSand" />
               <p>{`Ingredientes: ${pizza.ingredients}`}</p>
               <p>{`R$ ${pizza.preço},00`}</p>
-              <p>+</p>
+              <button type='button'>+</button>
             </div>
           ))
       }
