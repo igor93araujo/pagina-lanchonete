9dc7982da02ac66154bb578f3e6d127151d85344
Jefferson Donizete do Nascimento <jdn1937@hotmail.com>
2023-01-06 14:51:02 -0300

    carrinho de compras

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -1,40 +1,12 @@
-import React from 'react';
-import Header from '../components/Header';
-import '../App.css';
+import React, { Component } from 'react'
 
-class Carrinho extends React.Component {
-  render () {
-    const {cartCounter, cartTotal,cartItem, removeFromCartList} = this.props
+export default class Carrinho extends Component {
+
+  render() {
+    
     return (
-      <div className='conteiner-full'>
-      <Header cartCounter = {cartCounter} cartTotal = { cartTotal }/>
-      
-        {cartItem.length === 1 ? <p className='noItems'>Não há itens no carrinho.</p>
-      :
-      <div className='conteiner-products'>
-      {
-        cartItem.filter((item,index) => (index>=1)).map((item) => (
-          <div className='item'>
-              <p>{item.name}</p>
-              <img src={item.img} alt="imagemSand" />
-             ( if (item.ingredientes === undefined) {
-                <p>Geladinho!</p>
-              } :
-              <p>{`Ingredientes: ${item.ingredients}`}</p>)
-              <span>R$</span>
-              <span>{item.price}</span>
-              <span>,00</span>
-              <button
-                type='button'
-                onClick={removeFromCartList}
-              >Remover do carrinho</button>
-            </div>
-        ))
-      }
-      </div>}
-      </div>
+      <div>Carrinho 1</div>
+
     )
   }
 }
-
-export default Carrinho;
